227c6164fbd562487fe3115df5dd869782019565
vionabalqis <46768222+vionabalqis@users.noreply.github.com>
2019-01-24 13:30:30 +0700

    Add files via upload
    Tugas mencari keliling lingkaran dan bujursangkar

---
+++ Lingkarannnn.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Lingkarannnn {
+        public static void main(String[] args){
+            Lingkarannnn a = new Lingkarannnn();
+            a.keliling();
+        }
+
+
+        public int d = 5;
+        public double pi = 22/7;
+        public double kll;
+
+        public void keliling(){
+            kll = pi*d;
+            System.out.println("keliling lingkaran = " + kll);
+        }
+    }
+
+
---
+++ bujursangkarrrr.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class bujursangkarrrr {
+        public static void main(String[] args) {
+            bujursangkarrrr a = new bujursangkarrrr();
+            a.keliling();
+        }
+
+
+        public int sisi = 5;
+        public double kll;
+
+        public void keliling(){
+            kll = 4*sisi;
+            System.out.println("keliling bujur sangkar = " + kll);
+        }
+    }
